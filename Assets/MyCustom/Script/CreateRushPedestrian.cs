24feced9dc191373ddac3d3ad06160b37e90cd70
wsp50317 <wsp50317@gmail.com>
2019-05-20 10:41:42 +0800

    Add Pedestrian rush test

---
+++ Assets/MyCustom/Script/CreateRushPedestrian.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateRushPedestrian : MonoBehaviour {
+
+	public bool IsOn;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ChangeState()
+	{
+		if(IsOn)
+		{
+			IsOn = false;
+		}
+		else
+		{
+			IsOn = true;
+		}
+	}
+}
---
+++ Assets/MyCustom/Script/RushControl.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class RushControl : MonoBehaviour {
+	public Transform EndPoint;
+
+	private NavMeshAgent agent;
+	private Animator anim;
+	private Vector3 InitEndPos;
+	public bool IsTriggered = false;
+	private bool isTriggerChange = false;
+	private bool IsArrived = false;
+	
+
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		anim = GetComponentInChildren<Animator>();
+		InitEndPos = EndPoint.position;
+	}
+
+	void Update () {
+		if(IsTriggered)
+		{
+			if(!isTriggerChange)
+			{
+				anim.SetTrigger("Walk");
+				agent.SetDestination(InitEndPos);
+				isTriggerChange = true;
+			}
+
+			if(!IsArrived)
+			{
+				if (!agent.pathPending) //Judge that whether it arriving at EndPos:
+				{
+					if (agent.remainingDistance <= agent.stoppingDistance)
+					{
+						if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+						{
+							//If it arrived, do this thing:
+							anim.SetTrigger("Idle");
+							IsArrived = true;
+						}
+					}
+				 }
+			}
+		}
+	}
+}
+
