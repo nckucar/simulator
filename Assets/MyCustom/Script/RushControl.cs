27e9f936f3da89271b98ff67bbd9ca8a49cac4b5
wsp50317 <wsp50317@gmail.com>
2019-06-11 17:35:11 +0800

    Adjust the RushPedestrian. 1. The pedestrian will stop for seconds and keep walking. 2. Add NavMesh for the pedestrian's navigation. 3. Add the test function of rushing in NCKU_MKZ_V2. (The vehicle for rushing    test will remove in the future.)

--- Assets/MyCustom/Script/RushControl.cs
+++ Assets/MyCustom/Script/RushControl.cs
@@ -6,32 +6,67 @@ using UnityEngine.UI;
 
 public class RushControl : MonoBehaviour {
 	public Transform EndPoint;
+	public Transform FarPoint; //To keep walking
 
 	private NavMeshAgent agent;
 	private Animator anim;
 	private Vector3 InitEndPos;
+	private Vector3 FarPos;
 	public bool IsTriggered = false;
 	private bool isTriggerChange = false;
 	private bool IsArrived = false;
+	public bool NonStop;
+	public float WaitTime;
 	
+	private int MoveStage = 0;//0:Walking 1:Stop for a while  2:Walking again
 
 	void Start () {
 		agent = GetComponent<NavMeshAgent>();
 		anim = GetComponentInChildren<Animator>();
 		InitEndPos = EndPoint.position;
+		FarPos = FarPoint.position;
 	}
 
 	void Update () {
-		if(IsTriggered)
+		if(!NonStop) 
 		{
-			if(!isTriggerChange)
+			if(IsTriggered) //Set 1 by the vehicle.
 			{
+				if(!isTriggerChange) //Judge that whether the pedestrian walking.
+				{
+					anim.SetTrigger("Walk");
+					agent.SetDestination(InitEndPos);
+					isTriggerChange = true;
+				}
+
+				if(!IsArrived) //Seting by suring that it stops at the Endpoint.
+				{
+					if (!agent.pathPending) //Judge that whether it arriving at EndPos:
+					{
+						if (agent.remainingDistance <= agent.stoppingDistance)
+						{
+							if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+							{
+								//If it arrived, do this thing:
+								anim.SetTrigger("Idle");
+								IsArrived = true;
+							}
+						}
+					 }
+				}
+			}
+		}
+		else //NonStop mode
+		{
+			if(!isTriggerChange) //Judge that whether the pedestrian walking.
+			{
+				//Walking
 				anim.SetTrigger("Walk");
 				agent.SetDestination(InitEndPos);
 				isTriggerChange = true;
 			}
 
-			if(!IsArrived)
+			if(!IsArrived) //Seting by suring that it stops at the Endpoint.
 			{
 				if (!agent.pathPending) //Judge that whether it arriving at EndPos:
 				{
@@ -39,14 +74,22 @@ public class RushControl : MonoBehaviour {
 					{
 						if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
 						{
-							//If it arrived, do this thing:
+							//If it arrived at Endpoint, do this thing:
 							anim.SetTrigger("Idle");
+							StartCoroutine("IdleForSeconds", WaitTime);
 							IsArrived = true;
 						}
 					}
 				 }
 			}
 		}
+	}
+	IEnumerator IdleForSeconds(float sec)
+	{
+		anim.SetTrigger("Idle");
+		yield return new WaitForSeconds(sec);
+		anim.SetTrigger("Walk");
+		agent.SetDestination(FarPos);
 	}
 }
 
